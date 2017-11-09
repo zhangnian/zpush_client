23b5db173607c660afb02f8bf060164e9f98be4f
zhangnian <zhangnian@91jianke.com>
2017-11-09 19:10:36 +0800

    fix

--- main.go
+++ main.go
@@ -6,9 +6,23 @@ import (
 	"io"
 	"log"
 	"net"
-	msg "zpush_client/message"
 	"time"
+	msg "zpush_client/message"
 )
+
+func buildMsg(cmd int, b []byte) []byte{
+	buf := make([]byte, 2+4+len(b))
+
+	n := 0
+	binary.BigEndian.PutUint16(buf[n:], uint16(cmd))
+	n += 2
+
+	binary.BigEndian.PutUint32(buf[n:], uint32(len(b)))
+	n += 4
+
+	copy(buf[n:], b)
+	return buf
+}
 
 func mockLogin(username, password string) {
 	loginReq := msg.LoginReq{
@@ -21,22 +35,14 @@ func mockLogin(username, password string) {
 		log.Fatalf("marshal client msg error: %s\n", err.Error())
 	}
 
-	buf := make([]byte, 2+4+len(b))
+	buf := buildMsg(1, b)
 
-	n := 0
-	binary.BigEndian.PutUint16(buf[n:], 1)
-	n += 2
-
-	binary.BigEndian.PutUint32(buf[n:], uint32(len(b)))
-	n += 4
-
-	copy(buf[n:], b)
-
-	conn, err := net.Dial("tcp", "192.168.123.199:10001")
+	conn, err := net.Dial("tcp", "localhost:1001")
 	if err != nil {
 		log.Fatalln("connect to gateway error")
 	}
 	defer conn.Close()
+
 
 	r, err := conn.Write(buf)
 	if err != nil {
@@ -57,7 +63,58 @@ func mockLogin(username, password string) {
 		return
 	}
 
-	log.Println(packetHeaderBuf)
+	packetBodyLen := binary.BigEndian.Uint32(packetHeaderBuf[2:6])
+
+	packetBodyBuf := make([]byte, packetBodyLen)
+	_, err = io.ReadFull(conn, packetBodyBuf)
+	if err != nil {
+		if err == io.EOF {
+			log.Println("connection closed by peer")
+			return
+		}
+
+		log.Printf("io.ReadFull error: %s\n", err.Error())
+		return
+	}
+	var loginResp msg.LoginResp
+	err = proto.Unmarshal(packetBodyBuf, &loginResp)
+	if err != nil {
+		log.Println("unmarshal error", err.Error())
+		return
+	}
+
+	for {
+		mockHeartBeat(conn, loginResp.Userid)
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func mockHeartBeat(conn net.Conn, userid int32) {
+	hbReq := msg.HBReq{
+		Userid: userid,
+	}
+
+	b, _ := proto.Marshal(&hbReq)
+	buf := buildMsg(9999, b)
+
+	_, err := conn.Write(buf)
+	if err != nil {
+		log.Fatalln("write error")
+	}
+
+	//log.Printf("send heartbeat message to gateway, size: %d\n", r)
+
+	packetHeaderBuf := make([]byte, 6)
+	_, err = io.ReadFull(conn, packetHeaderBuf)
+	if err != nil {
+		if err == io.EOF {
+			log.Println("connection closed by peer")
+			return
+		}
+
+		log.Printf("io.ReadFull error: %s\n", err.Error())
+		return
+	}
 
 	packetBodyLen := binary.BigEndian.Uint32(packetHeaderBuf[2:6])
 
@@ -73,26 +130,16 @@ func mockLogin(username, password string) {
 		return
 	}
 
-	log.Println(packetBodyBuf)
-
-	var loginResp msg.LoginResp
-	err = proto.Unmarshal(packetBodyBuf, &loginResp)
-	if err != nil{
+	hbResp := msg.HBResp{}
+	err = proto.Unmarshal(packetBodyBuf, &hbResp)
+	if err != nil {
 		log.Println("unmarshal error", err.Error())
 		return
-	}
-
-	log.Println(loginResp)
-
-	for{
-		tmpBuf := make([]byte, 1024)
-		conn.Read(tmpBuf)
-		log.Println(tmpBuf)
 	}
 }
 
 func main() {
-	for i:=0; i<10; i++{
+	for i := 0; i < 10; i++ {
 		go mockLogin("zhangnian", "123456")
 	}
 
